e0f8907fd63a10e43064f298fcc4fd2a07e046da
Bruna Ruback Frauches <brunaruback07@gmail.com>
2023-12-07 13:31:16 -0300

    correcao plot

--- Código/plot.py
+++ Código/plot.py
@@ -1,14 +1,10 @@
 import matplotlib.pyplot as plt
 
-# Sample data
 num_loads = [10, 20, 30, 40, 50, 100, 200, 500, 1000, 5000, 10000, 20000, 30000]
-load_times = [0.002, 0.0, 0.0, 0.0, 0.0, 0.001, 0.005, 0.042, 0.319, 39.7, 343.05, 4364.82, 11110]
+load_times = [0.002, 0.001, 0.0, 0.001, 0.001, 0.002, 0.009, 0.058, 0.341, 37.79, 337.21, 2438,76, 11110]
 
-# Plotting the line graph
 plt.plot(num_loads, load_times, marker='o', linestyle='-', color='blue')
 
-# Adding labels and title
 plt.title('Tempo médio de execução (segundos)')
 
-# Display the graph
 plt.show()
