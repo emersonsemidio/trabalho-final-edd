92844235364fe4844e97a53b2ac36ef22082b434
Emerson Emidio <emersons.emidio01@gmail.com>
2023-12-06 23:52:41 -0300

    alterações
    alterações trabalho final

--- Código/Main.java
+++ Código/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		final int numeroVertices = 50;
+		final int numeroVertices = 20;
 		OrdenacaoTopologica ord = new OrdenacaoTopologica();
 		//Colocar o caminho inteiro até o arquivo de entrada
 		String nomeEntrada = "C:\\Users\\Emidio\\Desktop\\Trabalho Final\\Código\\entrada.txt";
@@ -12,7 +12,7 @@ public class Main
 
 
 		//ord.realizaLeitura(nomeEntrada);
-		//ord.geraGrafo(numeroVertices, 0.04);
+		ord.geraGrafo(numeroVertices, 0.13);
 		//ord.incializar();
 		//ord.executa();
 
